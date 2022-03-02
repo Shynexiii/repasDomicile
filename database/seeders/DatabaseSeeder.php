2c84f206c181579c5af40ae393e8bce8ee42b913
shynexiii <habibaminou2@gmail.com>
2022-03-02 14:15:23 +0100

    Ajout d'un admin et client par defaut dans le seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,19 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->insert([
             'last_name' => $faker->lastName,
             'first_name' => $faker->firstName,
-            'email' => $faker->email,
+            'email' => 'admin@admin.com',
+            'role' => 'admin',
+            'phone' => '07' . $faker->randomNumber(8, true),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('users')->insert([
+            'last_name' => $faker->lastName,
+            'first_name' => $faker->firstName,
+            'email' => 'client@client.com',
             'role' => 'admin',
             'phone' => '07' . $faker->randomNumber(8, true),
             'email_verified_at' => now(),
@@ -37,7 +49,7 @@ class DatabaseSeeder extends Seeder
         $adresse = Adresse::factory()->make();
         $userX->adresse()->save($adresse);
 
-        User::factory(4)->create()->each(function ($user) {
+        User::factory(5)->create()->each(function ($user) {
             $adresse = Adresse::factory()->make();
             $user->adresse()->save($adresse);
         });
