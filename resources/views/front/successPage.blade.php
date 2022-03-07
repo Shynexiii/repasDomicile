b20c572a1be929fa7f14f0d5a74dfcb42a9370aa
shynexiii <habibaminou2@gmail.com>
2022-03-07 02:23:19 +0100

    Mise a jour la page de commande success

--- resources/views/front/successPage.blade.php
+++ resources/views/front/successPage.blade.php
@@ -3,14 +3,13 @@
 @section('title', 'Paiement réussi')
 
 @section('content')
-    <div class="d-flex justify-content-center mb-5 flex-column align-items-center">
-        <img src="{{ asset('images/correct.png') }}" alt="">
-        <span class="font-weight-bold"></span>
-        <h1>Thanks for your order!</h1>
-        <p>
-            We appreciate your business!
-            If you have any questions, please email
-            <a href="mailto:orders@example.com">orders@example.com</a>.
-        </p>
-    </div>
+<div class="d-flex justify-content-center mb-5 flex-column align-items-center">
+    <img src="{{ asset('images/correct.png') }}" alt="">
+    <span class="font-weight-bold"></span>
+    <h1>Merci pour votre commande!</h1>
+    <p>
+        Si vous avez des questions, veuillez envoyer un e-mail à
+        <a href="mailto:repasadomicile@evry.fr">repasadomicile@evry.fr</a>.
+    </p>
+</div>
 @endsection
